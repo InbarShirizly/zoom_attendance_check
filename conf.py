f40bc76e32ee1911b1328b1f0cbf9f0bec288000
Inbar Shirizly <in.shirizly@gmail.com>
2020-09-22 16:32:15 +0300

    addressed code review and added comments over each function

--- conf.py
+++ conf.py
@@ -2,30 +2,12 @@ import re
 import numpy as np
 
 
-# variables to change
-FILE_NAME = ".\chat files\meeting_example_full_name.txt"
-EXCEL_PATH = "דוגמה לרשימת תלמידים.xlsx"
-
-
-SENTENCE_START = "attendance check"
-TIME_DELTA = 6  # time period in minuts
-COLUMNS_NAMES = ["time", "users", "chat"]
 EXCEL_COLS = {"Name": "שם התלמיד",
               "ID": "תעודת זהות",
               "Phone": "טלפון"}
 
-WORDS_FOR_NOT_INCLUDED_PARTICIPATORS = ["ITC", "Tech", "Challenge"]
-
 
 # constants
-JSON_FILE_NAME = "zoom_names_to_full_names"
 CHAT_PATTERN = re.compile(r"(^\d{2}.\d{2}.\d{2})\s+From\s\s([\s\S]+)\s:\s([\s\S]+)")
-class_names = ['Andrey Bakhmat', 'Areej Eweida', 'Ariela Strimling',
-               'Ariella Berger', 'Aviv Kadair', 'Dana Makov', 'Daniel Kagan', 'Hanna Hier',
-               'Inbar Shirizly', 'Itamar Meimon', 'Jonathan Bitton', 'Kevin Daniels', 'Limor Nunu',
-               'Lucas Gilbert Bensaid', 'May Lev Steinfeld', 'Michael Marcus', 'Michael Goncharov',
-               'Nicolas Mai', 'Nir Barazida', 'Ori Kleinhauz', 'Ron Zehavi', 'Rozanna Royter',
-               'Sebastian Kleiner', 'Serah Abensour', 'Sheryl Sitruk', 'Tal Toledano',
-               'Tammuz Dubnov', 'Yuval Herman']
 
 
--- zoom_chat_check_attendence.py
+++ zoom_chat_check_attendence.py
@@ -2,14 +2,14 @@
 ----------- zoom check attendance -----------
 Program implementations:
 a. gets a text file - zoom chat from class
-b. find when an attendance check has been initiated and look for all the participators who wrote
-something in the chat before or after the check
-c. finds the connection between the zoom name and the student full name (Could be from a cache of these connections
-in a json file, after getting all the connections, update the json file with all the new occurrences (if were some)
-d. prints all the missing students by subtracting the participators from the total students in class
-e. this program can run over multiple attendance check in the same file
+b. find session when an attendance check has been initiated and look for all the participators who wrote
+    something meaningful in the chat before or after the check. function checks allows user to write a name of the
+    student list only once. in case user had a mistake, it will check all his messages until it find the relevant one
+c. return df contains all the users from the csv/excel file with new columns that have: zoom name/ Nan - where zoom name
+    means that this zoom user added the relevant name and Nan means missing student
+d. retrun df from each session with the messages of zoom users that did not write something related to the actuall name
+    (allows the teacher to check if they had type error and then add them manually)
 
-a config file is attached to this program with all the related variables
 """
 
 from datetime import datetime
@@ -18,46 +18,61 @@ import pandas as pd
 import re
 import conf
 
+# configuration variables
+FILE_NAME = ".\chat files\meeting_example_full_name.txt"
+EXCEL_PATH = "דוגמה לרשימת תלמידים.xlsx"
+SENTENCE_START = "attendance check"
+TIME_DELTA = 1  # time period in minuets
+WORDS_FOR_NOT_INCLUDED_PARTICIPATORS = ["ITC", "Tech", "Challenge"]
 
-class attendance:
 
-    def __init__(self, chat_path, filter_modes):
+class Attendance:
+
+    def __init__(self, chat_path, student_file_path, filter_modes, time_delta, start_sentence):
         """
-        convert the chat text file to a data frame.
-        change time to date and remove "\n" from the chat
+        - convert the chat text file to a data frame and arrange columns.
+        - creates df for each session according the appearance of the start sentence and time delta
+        - create df from the excel or csv file of the studnets
         :param chat_path: the path of the relevant chat file (str)
+        :param student_file_path: path for the relevant csv/excel file (str)
+        :param filter_modes: filters the user picked for parsing the text file (list of str)
+        :param time_delta: max time from start sentence to the last message to parse in each session in minutes (int)
+        :param start_sentence: start sentence that initiate sessions for parse (str)
         :return: data frame with the data from the chat
         """
         with open(chat_path, "r", encoding="utf-8") as file:
             data = [re.search(conf.CHAT_PATTERN, line).groups() for line in file if re.match(conf.CHAT_PATTERN, line)]
-        df = pd.DataFrame(data, columns=conf.COLUMNS_NAMES)
+        df = pd.DataFrame(data, columns=["time", "users", "chat"])
         df['chat'] = df['chat'].str[:-1]
         df["time"] = df["time"].apply(lambda string: datetime.strptime(string, "%H:%M:%S"))
 
-        self.full_chat_df = df
-        start_indices = df.index[df['chat'].apply(lambda string: conf.SENTENCE_START.lower() in string.lower())]
-        self.time_delta = np.timedelta64(conf.TIME_DELTA, 'm')
-        self.df_sessions = [self.get_df_of_time_segment(start_index) for start_index in start_indices]
+        start_indices = df.index[df['chat'].apply(lambda string: start_sentence.lower() in string.lower())]
+
+        self.df_sessions = [self.get_df_of_time_segment(df, start_index, time_delta) for start_index in start_indices]
         self.filter_modes = filter_modes
-        self.df_students = pd.read_excel(conf.EXCEL_PATH, usecols=conf.EXCEL_COLS.values()).astype("str")
-        self.attendance_df = self.df_students
+        if student_file_path.endswith(".csv"):
+            self.df_students = pd.read_csv(student_file_path, usecols=conf.EXCEL_COLS.values()).astype("str")
+        else:
+            self.df_students = pd.read_excel(student_file_path, usecols=conf.EXCEL_COLS.values()).astype("str")
 
-    def get_df_of_time_segment(self, start_index):
-        time_segment_start = self.full_chat_df.loc[start_index, "time"]
-        time_filt = (self.full_chat_df["time"] >= time_segment_start) & \
-                    (self.full_chat_df["time"] <= time_segment_start + self.time_delta)
+    @staticmethod
+    def get_df_of_time_segment(df, start_index, time_delta):
+        
+        time_delta = np.timedelta64(time_delta, 'm')
+        time_segment_start = df.loc[start_index, "time"]
+        time_filt = (df["time"] >= time_segment_start) & \
+                    (df["time"] <= time_segment_start + time_delta)
 
-        return self.full_chat_df.loc[time_filt]
+        return df.loc[time_filt]
 
 
     def get_participants(self, df_chat):
         """
-        parse the data frame of the chat data to find the participants in the meeting that
-        wrote something in a period of time after a specific sentence had been writen
-        zoom user with name that will be probably the lecturer is deleted
-        :param df_chat: data frame of the chat content
-        :param start_index: start index of session
-        :return: participants in the zoom meeting (exclude lecturer) - (list_
+        finds students that attendant to the session. runs over each mode which represent different way to declare that
+        the student attendant (for example: phone number, ID). merges this data to the csv table with the zoom name that
+        added it
+        :param df_chat: that table of the chat for the specific session
+        :return: df of the attendance in the session
         """
         final_df = None
         for mode in self.filter_modes:
@@ -75,27 +90,47 @@ class attendance:
         return df_participated_updated
 
     @staticmethod
-    def get_zoom_users_not_included(df_participated, df_chat):
+    def get_zoom_users_not_included(df_participated, df_chat, not_included_part):
+        """
+        get the zoom users who wrote something in the chat but it was not related to the csv/excel users info
+        - drops not relevant records from the df of zoom users that are not part of the class
+        - drops the zoom users who wrote something meaningful
+        :param df_participated: output of participators - that contains Nan for csv student that weren't mentioned (df)
+        :param df_chat: full chat of the relevant session (df)
+        :param not_included_part: zoom user name that will not be included (list of str)
+        :return: table of zoom users and there messages in the session (df)
+        """
         filt = ~(df_chat['users'].isin(df_participated['users'].dropna())) & \
-               ~(df_chat['users'].str.contains('|'.join(conf.WORDS_FOR_NOT_INCLUDED_PARTICIPATORS)))
+               ~(df_chat['users'].str.contains('|'.join(not_included_part)))
         df_zoom_not_correct = df_chat[filt]
         return df_zoom_not_correct.drop(columns=["time"]).set_index("users")
 
-    def get_attendance(self):
-        df_zoom_not_correct_list  = []
-        for i in range(len(self.df_sessions)):
-
-            df_participated = self.get_participants(self.df_sessions[i])
-            self.attendance_df[f'session {i + 1}'] = df_participated['users']
-            df_zoom_not_correct = self.get_zoom_users_not_included(df_participated, self.df_sessions[i])
+    def get_attendance(self, not_included_part):
+        """
+        - get table of attendant student from the csv/excel file and list of table of relevant data from zoom users that
+          didn't add a student.
+        - runs over all the session, for each one take the zoom user names and add it as a column to the final df -
+          if there is Nan it means that the student was missing in that session. the zoom user name is the user who added
+          the specific student.
+        - for each session append to a list all the df represent table of relevant data from zoom users that
+          didn't add a student
+        :param not_included_part: zoom user name that will not be included (list of str) 
+        :return: 1. table of attendant student from the csv/excel file,
+                 2. list of table of relevant data from zoom users that didn't add a student
+        """
+        df_zoom_not_correct_list = []
+        attendance_df = self.df_students
+        for i, session in enumerate(self.df_sessions):
+            df_participated = self.get_participants(session)
+            attendance_df[f'session {i + 1}'] = df_participated['users']
+            df_zoom_not_correct = self.get_zoom_users_not_included(df_participated, session, not_included_part)
             df_zoom_not_correct_list.append(df_zoom_not_correct)
 
-        return self.attendance_df, df_zoom_not_correct_list
+        return attendance_df, df_zoom_not_correct_list
 
 def main():
-    my_class = attendance(conf.FILE_NAME, ['Name', "ID", "Phone"])
-
-    attendance_df, df_zoom_not_correct_list = my_class.get_attendance()
+    my_class = Attendance(FILE_NAME, EXCEL_PATH, ['Name', "ID", "Phone"], TIME_DELTA, SENTENCE_START)
+    attendance_df, df_zoom_not_correct_list = my_class.get_attendance(WORDS_FOR_NOT_INCLUDED_PARTICIPATORS)
 
     print(attendance_df)
 
