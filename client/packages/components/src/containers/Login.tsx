e891a358ede3ad899d5b29f44dbd5847433665c7
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-03 11:35:29 +0300

    Added react-router-dom

---
+++ client/packages/components/src/containers/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+export const Login = () => {
+  return (
+    <>
+      <Typography variant='h2'>
+        Login
+      </Typography>
+    </>
+  )
+}
--- client/packages/components/src/index.tsx
+++ client/packages/components/src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { CssBaseline } from '@material-ui/core'
+import { Container, CssBaseline } from '@material-ui/core'
 import { CustomAppBar } from './layout/AppBar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Login } from './containers/Login'
 
 export const Application = () => (
   <>
     <CssBaseline />
-    <CustomAppBar />
+    <Router>
+      <CustomAppBar />
+
+      <Container maxWidth='lg'>
+        <Switch>
+          <Route path='/'>
+            <Login />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   </>
 )
