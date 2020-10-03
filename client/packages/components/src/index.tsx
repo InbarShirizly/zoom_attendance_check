b2629e8baf96505dfc7943df44ad8a993f92b3fc
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-03 10:57:24 +0300

    Skeleton app bar

--- client/packages/components/src/index.tsx
+++ client/packages/components/src/index.tsx
@@ -1,3 +1,31 @@
 import React from 'react'
+import { CssBaseline, AppBar, Toolbar, Typography, makeStyles, Button } from '@material-ui/core'
 
-export const HelloWorld = () => <h1>Hello, world!</h1>
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1
+  }
+}))
+
+export const Application = () => {
+  const classes = useStyles()
+
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Typography variant='h6' className={classes.title}>
+            Zoom Attendence
+          </Typography>
+
+          <Button color='inherit'>Register</Button>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
