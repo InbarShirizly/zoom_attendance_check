d18f71880854f8168bf87aea43b690456dc68b1a
Itay Shirizly <it.shirizly@gmail.com>
2020-10-04 19:17:36 +0300

    Created handler for 404 errors

--- Server/Server/config.py
+++ Server/Server/config.py
@@ -4,7 +4,6 @@ class FlaskConfig:
 
 
 class ParseConfig:
-
     FILE_COLS_DICT = {
         "name": ["שם התלמיד", "תלמידים", "שמות", "שם", "סטודנט"],
         "id_number": ["תעודת זהות", "ת.ז.", "ת.ז", "תז"],
@@ -14,3 +13,12 @@ class ParseConfig:
     }
     MASHOV_COLS = ["name", "org_class"]
     GENDER_DICT = {1: ["זכר", "ז", "(ז)"], 0: ["נקבה", "נ", "(נ)"]}
+
+
+class RestErrors:
+    INVALID_ROUTE = "Route does't exist"
+    INVALID_CLASS = "Invalid class id"
+    INVALID_REPORT = "Invalid report id"
+    INVALID_STATUS = "Invalid status id"
+    USERNAME_TAKEN = "Username already taken"
+    EMAIL_TAKEN = "Email already taken"
