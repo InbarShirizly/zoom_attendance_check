ea9cc3cdc74832402dc9eff2eb8f2eeb730db562
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-05 15:34:24 +0300

    Added rtl state context and dispatch

--- client/packages/components/src/providers/RtlProvider.tsx
+++ client/packages/components/src/providers/RtlProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useReducer } from 'react'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core'
 
 export enum TextDirection {
@@ -13,19 +13,49 @@ const themeWithDirection = (direction: TextDirection) => createMuiTheme({
   direction
 })
 
-export const RtlContext = createContext(TextDirection.LTR)
+type Action = { type: 'SET_RTL' } | { type: 'SET_LTR' }
+type Dispatch = (action: Action) => void
+
+const RtlStateContext = createContext<TextDirection | undefined>(undefined)
+const RtlDispatchContext = createContext<Dispatch | undefined>(undefined)
 
 interface RtlProviderProps {
   children: React.ReactNodeArray
 }
 
+const rtlReducer = (state: TextDirection, action: Action) => {
+  switch (action.type) {
+    case 'SET_LTR':
+      return TextDirection.LTR
+    case 'SET_RTL':
+      return TextDirection.RTL
+    default:
+      return state
+  }
+}
+
 export const RtlProvider = ({ children }: RtlProviderProps) => {
-  const direction = useContext(RtlContext)
-  const theme = themeWithDirection(direction)
+  const [state, dispatch] = useReducer(rtlReducer, TextDirection.LTR)
+  const theme = themeWithDirection(state)
 
   return (
     <ThemeProvider theme={theme}>
-      {children}
+      <RtlStateContext.Provider value={state}>
+        <RtlDispatchContext.Provider value={dispatch}>
+          {children}
+        </RtlDispatchContext.Provider>
+      </RtlStateContext.Provider>
     </ThemeProvider>
   )
+}
+
+export const useRtlContext = () => {
+  const state = useContext(RtlStateContext)
+  const dispatch = useContext(RtlDispatchContext)
+
+  if (!state || !dispatch) {
+    throw new Error('useRtlContext must be used within a RtlProvider')
+  }
+
+  return { state, dispatch }
 }
