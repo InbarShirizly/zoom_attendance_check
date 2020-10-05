a90a3a5a21437a99a813f83400c1364811802878
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-05 12:13:20 +0300

    Roboto -> Heebo

--- client/packages/components/src/index.tsx
+++ client/packages/components/src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { CssBaseline } from '@material-ui/core'
+import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core'
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import { CustomAppBar } from './layout/AppBar'
 import { Container } from './layout/Container'
 import { Login, Register, Classes } from './containers'
 import { WithTranslateProps } from './external-types'
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Heebo", "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif'
+  }
+})
+
 export const Application = (i18nProps: WithTranslateProps) => (
-  <>
+  <ThemeProvider theme={theme}>
     <CssBaseline />
     <Router>
       <CustomAppBar {...i18nProps} />
@@ -32,5 +38,5 @@ export const Application = (i18nProps: WithTranslateProps) => (
         </Switch>
       </Container>
     </Router>
-  </>
+  </ThemeProvider>
 )
