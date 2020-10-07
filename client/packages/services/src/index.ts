46d027a174bb5211a5722788523418faf08894a0
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-07 14:32:55 +0300

    Created blueprint service

--- client/packages/services/src/index.ts
+++ client/packages/services/src/index.ts
@@ -1,3 +1,5 @@
+import ky from 'ky-universal'
+
 export interface StudentData {
   id: number
   classId: number
@@ -17,4 +19,33 @@ export interface Classroom {
   name: string
   id: number
   students: StudentData[]
+}
+
+type ShallowClassroom = Pick<Classroom, 'id' | 'name'>
+
+interface ClientOptions {
+  baseUrl: string
+}
+
+export const createServiceClient = ({ baseUrl }: ClientOptions) => {
+  const getClassrooms = () => {}
+
+  const getClassroomById = (id: number) => {}
+
+  const createClassroom = (classroom: Classroom) => {}
+
+  const changeClassroom = (id: number, newName: string) => {}
+
+  const deleteClassroom = (id: number) => {}
+
+  const deleteAllClassrooms = () => {}
+
+  return {
+    getClassrooms,
+    createClassroom,
+    changeClassroom,
+    deleteClassroom,
+    getClassroomById,
+    deleteAllClassrooms
+  }
 }
