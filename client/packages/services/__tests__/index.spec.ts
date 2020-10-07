8712c03ebdfbaf592a6de425681ed022de18081b
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-07 15:07:59 +0300

    Added failing tests

--- client/packages/services/__tests__/index.spec.ts
+++ client/packages/services/__tests__/index.spec.ts
@@ -1,3 +1,36 @@
-test('foo', async () => {
-  console.log('Dummy')
+import { createTestkit } from './testkit'
+import { createServiceClient } from '../src/index'
+
+const baseUrl = 'http://example.com'
+
+describe('Service tests', () => {
+  const testkit = createTestkit({ baseUrl })
+  const service = createServiceClient({ baseUrl })
+
+  it('should return a classroom by id', async () => {
+    const someClassroom = {
+      id: 1,
+      name: 'Some Class',
+      students: []
+    }
+
+    testkit.addClassroom(someClassroom)
+
+    const classroom = await service.getClassroomById(someClassroom.id)
+    expect(classroom).toEqual(someClassroom)
+  })
+
+  it('should return all classrooms', async () => {
+    const someClassroom = {
+      id: 1,
+      name: 'Some Class',
+      students: []
+    }
+
+    testkit.addClassroom(someClassroom)
+
+    const classrooms = await service.getClassrooms()
+    expect(classrooms).toHaveLength(1)
+    expect(classrooms[0]).toEqual({ id: someClassroom.id, name: someClassroom.name })
+  })
 })
