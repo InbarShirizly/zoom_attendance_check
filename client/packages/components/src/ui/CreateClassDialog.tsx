1030265ff5735c1e079b0651d5347e69202c9602
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-09 01:05:09 +0300

    Added form submit handler to create class dialog

--- client/packages/components/src/ui/CreateClassDialog.tsx
+++ client/packages/components/src/ui/CreateClassDialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Button, Dialog, DialogActions, DialogContent, DialogProps, DialogTitle, makeStyles, TextField } from '@material-ui/core'
 import { FileButton } from './FileButton'
 
@@ -10,11 +10,29 @@ const useStyles = makeStyles(theme => ({
 
 interface CreateClassDialogProps extends DialogProps {
   open: boolean
-  onFileChange(file: File): void
+  onFormSubmit(name: string, file: File): void
 }
 
-export const CreateClassDialog = ({ open = false, onClose, onFileChange }: CreateClassDialogProps) => {
+export const CreateClassDialog = ({ open = false, onClose, onFormSubmit }: CreateClassDialogProps) => {
   const classes = useStyles()
+  const [studentsFile, setStudentFile] = useState<File>()
+  const [className, setClassName] = useState<string>()
+
+  const handleFileChange = useCallback((file: File) => {
+    setStudentFile(file)
+  }, [setStudentFile])
+
+  const handleNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setClassName(event.target.value)
+  }, [setClassName])
+
+  const handleSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    if (className && studentsFile) {
+      onFormSubmit(className, studentsFile)
+    }
+  }, [studentsFile, className])
 
   return (
     <Dialog open={open} onClose={onClose} fullWidth maxWidth='sm'>
@@ -22,16 +40,17 @@ export const CreateClassDialog = ({ open = false, onClose, onFileChange }: Creat
         Create class
       </DialogTitle>
       <DialogContent>
-        <form>
+        <form onSubmit={handleSubmit}>
           <TextField
             label='Class name'
             className={classes.nameInput}
             fullWidth
+            onChange={handleNameChange}
           />
           <FileButton
             id='upload-students-file'
             label='Upload students file'
-            onFileChange={onFileChange}
+            onFileChange={handleFileChange}
           />
         </form>
       </DialogContent>
