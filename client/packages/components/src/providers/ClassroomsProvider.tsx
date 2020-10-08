0483a1fa7b78001fb3eeeb14659f8aa296467295
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-09 00:54:24 +0300

    Added classroom actions

--- client/packages/components/src/providers/ClassroomsProvider.tsx
+++ client/packages/components/src/providers/ClassroomsProvider.tsx
@@ -1,8 +1,9 @@
-import { ShallowClassroom } from 'services'
+import { Classroom, ShallowClassroom } from 'services'
 import { createProvider, Thunk } from './create-provider'
 
 interface ClassroomsState {
   classrooms: ShallowClassroom[]
+  selectedClassroom?: Classroom
 }
 
 type FetchAction = {
@@ -19,12 +20,32 @@ type ErrorAction = {
   error?: Error
 }
 
-type Action = FetchAction | SuccessfulFetchAction | ErrorAction
+type CreateAction = {
+  type: 'CREATE_CLASSROOM_START'
+}
+
+type SuccessfulCreateAction = {
+  type: 'CREATE_CLASSROOM_SUCCESS',
+  classroom: Classroom
+}
+
+type SelectClassroom = {
+  type: 'SELECT_CLASSROOM',
+  selectedClassroom: Classroom
+}
+
+type Action =
+  | FetchAction | SuccessfulFetchAction
+  | CreateAction | SuccessfulCreateAction
+  | SelectClassroom
+  | ErrorAction
 
 const classroomsReducer = (state: ClassroomsState, action: Action) => {
   switch (action.type) {
     case 'FETCH_CLASSROOMS_SUCCESS':
       return { ...state, classrooms: action.classrooms }
+    case 'CREATE_CLASSROOM_SUCCESS':
+      return { ...state, selectedClassroom: action.classroom }
     case 'FETCH_ERROR':
       return { ...state, classrooms: [] }
     default:
