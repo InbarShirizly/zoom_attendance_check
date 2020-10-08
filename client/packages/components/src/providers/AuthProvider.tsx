1b7dd9ddbe3e3cb56af420afd7821ee5337ab102
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-08 14:15:23 +0300

    Added register action

--- client/packages/components/src/providers/AuthProvider.tsx
+++ client/packages/components/src/providers/AuthProvider.tsx
@@ -3,22 +3,51 @@ import { createProvider, Thunk } from './create-provider'
 interface AuthState {
   token?: string
   failed: boolean
+  faliureReason?: string
 }
 
-type LoginAction = { type: 'LOGIN', usernam: string, password: string }
-type LogoutAction = { type: 'LOGOUT' }
-type SuccessAction = { type: 'AUTH_SUCCESS', token: string }
-type FailAction = { type: 'AUTH_FAILED' }
-type Action = LoginAction | LogoutAction | SuccessAction | FailAction
+type LoginAction = {
+  type: 'LOGIN',
+  username: string,
+  password: string
+}
+
+type RegisterAction = {
+  type: 'REGISTER',
+  username: string,
+  password: string,
+  confirmPasword: string,
+  email: string
+}
+
+type LogoutAction = {
+  type: 'LOGOUT'
+}
+
+type SuccessAction = {
+  type: 'AUTH_SUCCESS',
+  token: string
+}
+
+type FailAction = {
+  type: 'AUTH_FAILED'
+  error?: Error
+}
+
+type Action = LoginAction | LogoutAction | SuccessAction | FailAction | RegisterAction
 
 const authReducer = (state: AuthState, action: Action) => {
   switch (action.type) {
     case 'AUTH_SUCCESS':
       return { ...state, token: action.token, failed: false }
     case 'AUTH_FAILED':
-      return { ...state, failed: true }
+      return { ...state, failed: true, failureReason: action.error?.message }
     case 'LOGOUT':
       return { failed: false }
+    case 'REGISTER':
+      return action.password !== action.confirmPasword
+        ? { ...state, failed: true, failureReason: 'password_mismatch' }
+        : state
     default:
       return state
   }
