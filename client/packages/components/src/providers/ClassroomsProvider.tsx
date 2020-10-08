24248427347c20a96060ae8497d7d0193c9c55a6
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-08 16:20:52 +0300

    Added more actions

--- client/packages/components/src/providers/ClassroomsProvider.tsx
+++ client/packages/components/src/providers/ClassroomsProvider.tsx
@@ -1,19 +1,32 @@
-import { Classroom } from 'services'
+import { ShallowClassroom } from 'services'
 import { createProvider, Thunk } from './create-provider'
 
 interface ClassroomsState {
-  classrooms: Classroom[]
+  classrooms: ShallowClassroom[]
 }
 
 type FetchAction = {
-  type: 'FETCH_CLASSROOMS',
-  token: string
+  type: 'FETCH_CLASSROOMS_START'
 }
 
-type Action = FetchAction
+type SuccessfulFetchAction = {
+  type: 'FETCH_CLASSROOMS_SUCCESS',
+  classrooms: ShallowClassroom[]
+}
+
+type ErrorAction = {
+  type: 'FETCH_ERROR',
+  error?: Error
+}
+
+type Action = FetchAction | SuccessfulFetchAction | ErrorAction
 
 const classroomsReducer = (state: ClassroomsState, action: Action) => {
   switch (action.type) {
+    case 'FETCH_CLASSROOMS_SUCCESS':
+      return { ...state, classrooms: action.classrooms }
+    case 'FETCH_ERROR':
+      return { ...state, classrooms: [] }
     default:
       return state
   }
