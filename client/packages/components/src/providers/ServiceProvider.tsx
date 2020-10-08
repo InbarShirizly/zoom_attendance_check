164da242cf1e53960d83badc523601892bdbbe5d
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-08 16:29:47 +0300

    Added set token action

--- client/packages/components/src/providers/ServiceProvider.tsx
+++ client/packages/components/src/providers/ServiceProvider.tsx
@@ -1,13 +1,29 @@
-import { createServiceClient } from 'services'
+import { createServiceClient, Service } from 'services'
 import { createProvider } from './create-provider'
 
 const BASE_URL = 'http://localhost:5000'
 
 const service = createServiceClient({ baseUrl: BASE_URL })
 
+type Action = {
+  type: 'SET_TOKEN',
+  token?: string
+}
+
+const reducer = (state: Service, action: Action) => {
+  switch (action.type) {
+    case 'SET_TOKEN':
+      return action.token
+        ? createServiceClient({ baseUrl: BASE_URL, token: action.token })
+        : state
+    default:
+      return state
+  }
+}
+
 const {
   Provider: ServiceProvider,
   useProvider: useService
-} = createProvider('Service', _ => _, service)
+} = createProvider('Service', reducer, service)
 
 export { ServiceProvider, useService }
