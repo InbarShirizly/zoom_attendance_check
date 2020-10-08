e9d118c3f71424e5c263859bea1c888d78fe8e17
Jonathan Ohayon <jonathanohayon0@gmail.com>
2020-10-09 00:47:33 +0300

    Fixed redirect

--- client/packages/components/src/index.tsx
+++ client/packages/components/src/index.tsx
@@ -24,7 +24,7 @@ const Routes = (i18nProps: WithTranslateProps) => {
   return (
     <Switch>
       <Route path='/login'>
-        {authState.token ? <Classes /> : <Login {...i18nProps} />}
+        {authState.token ? <Redirect to='/home' /> : <Login {...i18nProps} />}
       </Route>
 
       <Route path='/register'>
