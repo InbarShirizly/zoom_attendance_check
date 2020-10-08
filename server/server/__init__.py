8fee8b90dddc043466cc8feeb3ad4f7040083abf
Inbar Shirizly <in.shirizly@gmail.com>
2020-10-08 14:56:02 +0300

    added flask cores

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -3,9 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from server.config import FlaskConfig
 from flask_httpauth import HTTPTokenAuth
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.config.from_object(FlaskConfig)
+CORS(app)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
